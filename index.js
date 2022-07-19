c05cea7917f2b473a8186888e51d6d25f5ee7c04
Marcelle Vargas <marcellevargas@Marcelle-Vargas-MacBook-Pro-464A.local>
2022-07-19 09:31:32 -0300

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=> res.send('Teste'));
+app.get('/', (req, res)=> res.json({data: "teste"}));
 
 app.listen(process.env.PORT || 3000)
